0850bb6e2dcb4454b6dda2a15c62e1195bb797c9
c_Door in <egor1@mail.ru>
2022-09-09 16:55:43 +0300

    Optomize file question adding

--- bot_utils/questions.py
+++ bot_utils/questions.py
@@ -1,22 +1,19 @@
 from pathlib import Path
 
 
-def add_file_questions(exist_questions, file_content):
+def get_file_questions(file_content):
     while '\n\n\n' in file_content:
         file_content = file_content.replace('\n\n\n', '\n\n')
+    questions_to_add = {}
     question = None
     for block in file_content.split('\n\n'):
         if 'Вопрос' in block:
             question = ' '.join(block.split('\n')[1:]).strip()
         if question and 'Ответ' in block:
             answer = ' '.join(block.split('\n')[1:]).strip()
-            try:
-                exist_questions[question] = answer
-            except UnboundLocalError as e:
-                print(block)
-                raise(e)
+            questions_to_add[question] = answer
             question = None
-    return exist_questions
+    return questions_to_add
             
 
 def get_questions():
@@ -24,7 +21,7 @@ def get_questions():
     for filepath in list(Path('./quiz-questions').glob('*.txt')):
         with open(str(filepath), 'r', encoding='KOI8-R') as file:
             file_content = file.read()
-        questions = add_file_questions(questions, file_content)
+        questions.update(get_file_questions(file_content))
     return questions
 
 
